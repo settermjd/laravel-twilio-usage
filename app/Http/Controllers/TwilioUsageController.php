c9637ec9142de296de92c7d2c400a162e2c4e58d
Matthew Setter <matthew@matthewsetter.com>
2023-02-27 12:55:55 +0100

    Add the ability to retrieve usage records for all time and today
    Originally, the app only retrieved usage records for the last month.
    This change allows them to be retrieved for all time (all records) and
    those from today.

--- app/Http/Controllers/TwilioUsageController.php
+++ app/Http/Controllers/TwilioUsageController.php
@@ -8,22 +8,30 @@ use App\Iterators\Filters\UsageWithCountFilter;
 use ArrayIterator;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Twilio\Rest\Api\V2010\Account\Usage\Record\LastMonthInstance;
 use Twilio\Rest\Client;
 
 class TwilioUsageController extends Controller
 {
+    public const DEFAULT_RECORD_LIMIT = 20;
+    public const DEFAULT_USAGE_TYPE = 'last_month';
+
     public function __construct(private readonly Client $twilio) {}
 
     /**
      * Retrieve and return the user's Twilio account usage information.
      */
-    public function __invoke(Request $request, int $recordLimit): View
+    public function __invoke(
+        Request $request,
+        int $recordLimit = self::DEFAULT_RECORD_LIMIT,
+        string $usageType = self::DEFAULT_USAGE_TYPE
+    ): View
     {
-        $usageRecords = $this->twilio
-            ->usage
-            ->records
-            ->lastMonth
-            ->read([], $recordLimit);
+        $usageRecords = match ($usageType) {
+            'all_time' => $this->getAllUsageRecords($recordLimit),
+            'today' => $this->getTodaysUsageRecords($recordLimit),
+            default => $this->getLastMonthUsageRecords($recordLimit),
+        };
 
         $recordsIterator = new ArrayIterator($usageRecords);
         $recordsIterator->uasort(function ($recordOne, $recordTwo) {
@@ -38,5 +46,35 @@ class TwilioUsageController extends Controller
                 'recordCount' => iterator_count($filteredRecords)
             ]
         );
+    }
+
+    /**
+     * @param int $recordLimit
+     * @return LastMonthInstance[]
+     */
+    public function getLastMonthUsageRecords(int $recordLimit): array
+    {
+        return $this->twilio
+            ->usage
+            ->records
+            ->lastMonth
+            ->read([], $recordLimit);
+    }
+
+    public function getTodaysUsageRecords(int $recordLimit): array
+    {
+        return $this->twilio
+            ->usage
+            ->records
+            ->today
+            ->read([], $recordLimit);
+    }
+
+    public function getAllUsageRecords(int $recordLimit): array
+    {
+        return $this->twilio
+            ->usage
+            ->records
+            ->read([], $recordLimit);
     }
 }
